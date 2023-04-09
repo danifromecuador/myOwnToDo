10f1fba043ec5c8427c0c6db57660cd018457f38
dani morillo <danifromecuador@gmail.com>
2023-04-08 19:04:24 -0500

    Create remove function

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 // import _ from 'lodash';
 // import './style.css';
-import { render, add} from './modules/functions.js';
-export let tasksContainerArray = JSON.parse(localStorage.getItem('tasksContainerArray')) || [];
+import { render, add, remove } from './modules/functions.js';
+let tasksContainerArray = JSON.parse(localStorage.getItem('tasksContainerArray')) || [];
 
 // ADD ////////////////////////////////////////////////////////////////////////////////////////////
 const addTaskBtn = document.querySelector('#addTaskBtn');
 addTaskBtn.addEventListener('click', () => {
   let userTaskInput = document.querySelector('#userTaskInput').value;
-  add(userTaskInput);
+  add(userTaskInput, tasksContainerArray);
 });
 
 const userTaskInput = document.querySelector('#userTaskInput');
 userTaskInput.addEventListener('keydown', (event) => {  
   let userTaskInput = document.querySelector('#userTaskInput').value;
   if (event.key === 'Enter') {
-    add(userTaskInput);
+    add(userTaskInput, tasksContainerArray);
   }
 });
+
+// REMOVE /////////////////////////////////////////////////////////////////////////////////////////
+export const deleteBtnListener = () => {
+  const deleteTaskBtnList = document.querySelectorAll('.deleteTaskBtn');
+  const deleteTaskBtnArray = Array.from(deleteTaskBtnList);
+  deleteTaskBtnList.forEach((deleteTaskBtn, index) => {
+    deleteTaskBtn.addEventListener('click', () => {
+      remove(index, tasksContainerArray);
+    });
+  });
+};
 
 // RENDER WHEN THE PAGE LOADS /////////////////////////////////////////////////////////////////////
 render(tasksContainerArray);
--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,4 +1,4 @@
-import { tasksContainerArray } from "../index.js";
+import { deleteBtnListener } from "../index.js";
 
 export const render = (tasksContainerArray) => {  
   const tasksContainer = document.querySelector('.tasksContainer');
@@ -12,12 +12,15 @@ export const render = (tasksContainerArray) => {
         <input type="button" value="delete" class="deleteTaskBtn">      
       </li>
       <hr>
-    `;
+    `;    
+    tasksContainerArray[i].index = i+1;
     tasksContainer.innerHTML += html;
-  }
+  }  
+  localStorage.setItem('tasksContainerArray', JSON.stringify(tasksContainerArray));
+  deleteBtnListener();
 };
 
-export const add = (userInput) => {
+export const add = (userInput, tasksContainerArray) => {
   if (userInput)  { // if user input is not empty so do following: 
     let userTaskInput = document.querySelector('#userTaskInput');
     userTaskInput.value = '';
@@ -31,4 +34,10 @@ export const add = (userInput) => {
     localStorage.setItem('tasksContainerArray', JSON.stringify(tasksContainerArray));
     render(tasksContainerArray);
   }
+};
+
+export const remove = (index, tasksContainerArray) => {
+  tasksContainerArray.splice(index, 1);
+  localStorage.setItem('tasksContainerArray', JSON.stringify(tasksContainerArray));
+  render(tasksContainerArray);
 };
